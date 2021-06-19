e983a8df076057db3c598a3dfc4728ba31e2f157
mario_chino98 <mario.martinez@accedo.tv>
2021-06-18 23:57:23 -0500

    feature: Adds sign in and sign up view

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
-import { Home, Shop } from "../views";
+import { Home, Shop, SigninAndSignup } from "../views";
 
 const Routes = () => (
   <Switch>
     <Route path="/shop" component={Shop} />
+    <Route exact path="/signin" component={SigninAndSignup} />
     <Route exact path="/" component={Home} />
   </Switch>
 );
---
+++ src/views/SigninAndSignup/SigninAndSignup.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+import SignIn from "../../components/Signin/Signin";
+
+const SigninAndSignup = () => (
+  <>
+    <div>SIGN IN</div>
+    <SignIn/>
+  </>
+);
+
+export default SigninAndSignup;
