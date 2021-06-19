15d0ab865fbf0f3af98eff7aa3533a54e30525a3
mario_chino98 <mario.martinez@accedo.tv>
2021-06-19 00:32:06 -0500

    feature: Includes Signin form

---
+++ src/components/shared/Input/Input.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import classNames from "classnames";
+import PropTypes from "prop-types";
+
+import theme from "./Input.module.scss";
+
+const Input = ({ label, onChange, value="", ...props }) => (
+  <div className={theme.inputContainer}>
+    <input className={theme.input} onChange={onChange} value={value}  {...props} />
+    { label && (
+      <label
+        className={classNames(theme.inputLabel, {
+          [theme.shrink]: !!value?.length
+        })}
+      >
+        {label}
+      </label>
+    ) }
+  </div>
+);
+
+Input.propTypes = {
+  label: PropTypes.string,
+  onChange: PropTypes.func,
+  value: PropTypes.string,
+  props: PropTypes.object
+}
+
+export default Input;
