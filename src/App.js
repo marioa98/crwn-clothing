7effd544008330959eb88a88c8a1e0af7e6c6850
mario_chino98 <mario.martinez@accedo.tv>
2021-08-04 16:58:16 -0500

    Sets user state from firebase auth observer

--- src/App.js
+++ src/App.js
@@ -1,14 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { BrowserRouter } from "react-router-dom";
 import "./App.scss";
 import Header from "./components/Header/Header";
 import Routes from "./routes";
+import { auth } from "./utils/firebase/firebase";
 
-const App = () => (
-  <BrowserRouter>
-    <Header />
-    <Routes />
-  </BrowserRouter>
-);
+const App = () => {
+  const [, setCurrentUser] = useState();
+
+  useEffect(() => {
+    /**
+     * Firebase observer that notifies for changes on the users' sign-in state and allows
+     * to persists user session.
+     * @see https://firebase.google.com/docs/reference/js/firebase.auth.Auth#onauthstatechanged
+     */
+    const unsuscribeFromAuth = auth.onAuthStateChanged((user) => setCurrentUser(user));
+
+    return () => {
+      unsuscribeFromAuth();
+    }
+  }, [])
+
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes />
+    </BrowserRouter>
+  );
+}
 
 export default App;
