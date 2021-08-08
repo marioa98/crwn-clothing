eb41fec25a2176371e56cc42d89a9fecca5cbac3
mario_chino98 <mario.martinez@accedo.tv>
2021-08-08 16:48:44 -0500

    feature: Add sign-in/sign-out option to Header based on user state

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from "prop-types";
 import { useTranslation } from "react-i18next";
 import { Link } from "react-router-dom";
 
@@ -6,7 +7,9 @@ import { ReactComponent as Logo } from "../../assets/logo.svg";
 import endpoints from "../../lib/endpoints.json";
 import theme from "./Header.module.scss";
 
-const Header = () => {
+import { auth } from "../../utils/firebase/firebase";
+
+const Header = ({ currentUser }) => {
   const { t } = useTranslation("menu");
 
   return (
@@ -22,9 +25,20 @@ const Header = () => {
         <Link className={theme.option} to={endpoints.contact}>
           {t("contact")}
         </Link>
+        {
+          currentUser ? (
+            <div className={theme.option} onClick={() => auth.signOut()}>{t("signOut")}</div>
+          ) : (
+            <Link className={theme.option} to={endpoints.signIn}>{t("signIn")}</Link>
+          )
+        }
       </div>
     </div>
   );
 };
+
+Header.propTypes = {
+  currentUser: PropTypes.object 
+}
 
 export default Header;
