a30b85c89345a65f2afd3310304e11fff9a8ff0b
mario_chino98 <mario.martinez@accedo.tv>
2021-11-13 14:50:51 -0600

    style: Includes inverted color style on Button

--- src/components/shared/Button/Button.jsx
+++ src/components/shared/Button/Button.jsx
@@ -4,20 +4,22 @@ import classNames from "classnames";
 
 import theme from "./Button.module.scss";
 
-const Button = ({ children, isGoogle = false, ...props }) => (
+const Button = ({ children, isGoogle = false, inverted, ...props }) => (
   <button
     className={classNames(theme.button, {
-      [theme.google]: isGoogle
+      [theme.google]: isGoogle,
+      [theme.inverted]: inverted
     })}
     {...props}
   >
-    {children}
+    {children} 
   </button>
 );
 
 Button.propTypes = {
   children: PropTypes.any,
   isGoogle: PropTypes.bool,
+  inverted: PropTypes.bool,
   props: PropTypes.object
 };
 
