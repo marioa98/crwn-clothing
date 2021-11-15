2938d428dbcb234cc713401fe3419e84f916eef8
mario_chino98 <mario.martinez@accedo.tv>
2021-11-15 17:20:46 -0600

    feature: Renders all selected items on cart dropdown

--- src/components/ShoppingCart/ShoppingCartDropdown.jsx
+++ src/components/ShoppingCart/ShoppingCartDropdown.jsx
@@ -1,18 +1,36 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 import { useTranslation } from 'react-i18next';
 import Button from '../shared/Button/Button';
 
 import theme from './ShoppingCartDropdown.module.scss';
+import ShoppingCartItem from '../ShoppingCartItem/ShoppingCartItem';
 
-const ShoppingCartDropdown = () => {
+const ShoppingCartDropdown = ({ items }) => {
   const { t } = useTranslation("cart");
 
   return (
     <div className={theme.dropdown}>
-      <div className={theme.items} />
+      <div className={theme.items}>
+        {
+          items.map((item) => <ShoppingCartItem key={item?.id} item={item} />)
+        }
+      </div>
       <Button>{t("checkout")}</Button>
     </div>
   )
 };
 
-export default ShoppingCartDropdown;
+ShoppingCartDropdown.propTypes = {
+  items: PropTypes.array
+}
+
+const mapStateToProps = (state) => {
+  const { cart } = state;
+  return {
+    items: cart.items
+  };
+};
+
+export default connect(mapStateToProps)(ShoppingCartDropdown);
