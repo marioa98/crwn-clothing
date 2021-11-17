34ddcf699a857d942e7fb2cac12958e548dc28f0
mario_chino98 <mario.martinez@accedo.tv>
2021-11-16 21:58:22 -0600

    feature: Cart counts total of items based on quantity not items on array

--- src/components/ShoppingCart/ShoppingCart.jsx
+++ src/components/ShoppingCart/ShoppingCart.jsx
@@ -1,32 +1,30 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { connect } from "react-redux";
+import { connect, useSelector } from "react-redux";
 import { ReactComponent as ShoppingBagIcon } from "../../assets/shopping-bag.svg";
 import { toggleCartHidden as toggleCartHiddenAction } from "../../redux/cart/actions";
 
 import theme from "./ShoppingCart.module.scss";
+import { selectTotalItems } from "../../redux/cart/selectors";
 
-const ShoppingCart = ({ toggleCartHidden, totalItems }) => (
-  <div className={theme.cartIcon} onClick={toggleCartHidden}>
-    <ShoppingBagIcon className={theme.shoppingIcon} />
-    <span className={theme.itemsCount}>{totalItems}</span>
-  </div>
-);
 
-ShoppingCart.propTypes = {
-  toggleCartHidden: PropTypes.func,
-  totalItems: PropTypes.number
+const ShoppingCart = ({ toggleCartHidden }) => {
+  const totalItems = useSelector(selectTotalItems);
+
+  return (
+    <div className={theme.cartIcon} onClick={toggleCartHidden}>
+      <ShoppingBagIcon className={theme.shoppingIcon} />
+      <span className={theme.itemsCount}>{totalItems}</span>
+    </div>
+  )
 };
 
-const mapStateToProps = (state) => {
-  const { cart } = state;
-  return {
-    totalItems: cart.items.length
-  };
+ShoppingCart.propTypes = {
+  toggleCartHidden: PropTypes.func
 };
 
 const mapDispatchToProps = (dispatch) => ({
   toggleCartHidden: () => dispatch(toggleCartHiddenAction())
 });
 
-export default connect(mapStateToProps, mapDispatchToProps)(ShoppingCart);
+export default connect(null, mapDispatchToProps)(ShoppingCart);
