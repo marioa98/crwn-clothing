ec7305079b3e43bb8ed13e6185d841f933c372d0
mario_chino98 <mario.martinez@accedo.tv>
2022-01-19 15:55:52 -0600

    feature: Able to increase/decrease quantity of item on checkout page

--- src/views/Checkout/CheckoutItem/CheckoutItem.jsx
+++ src/views/Checkout/CheckoutItem/CheckoutItem.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { connect } from "react-redux";
 
 import theme from "./CheckoutItem.module.scss";
 import toCurrency from "../../../lib/toCurrency";
+import { addItem as addItemAction, removeItem as removeItemAction } from "../../../redux/cart/actions";
 
-const CheckoutItem = ({ item, onRemove }) => {
+const CheckoutItem = ({ addItem, item, onRemove, removeItem }) => {
   const { imageUrl, name, quantity, price } = item;
   return (
     <div className={theme.checkoutItem}>
@@ -12,7 +14,11 @@ const CheckoutItem = ({ item, onRemove }) => {
         <img src={imageUrl} alt={name} />
       </div>
       <span className={theme.name}>{name}</span>
-      <span className={theme.quantity}>{quantity}</span>
+      <span className={theme.quantity}>
+        <div className={theme.arrow} onClick={() => removeItem(item)}>&#10094;</div>
+        <span className={theme.value}>{quantity}</span>
+        <div className={theme.arrow} onClick={() => addItem(item)}>&#10095;</div>
+      </span>
       <span className={theme.price}>{toCurrency(price)}</span>
       <div className={theme.removeButton} onClick={onRemove}>&#10005;</div>
     </div>
@@ -20,13 +26,20 @@ const CheckoutItem = ({ item, onRemove }) => {
 };
 
 CheckoutItem.propTypes = {
+  addItem: PropTypes.func,
   item: PropTypes.shape({
     imageUrl: PropTypes.string,
     name: PropTypes.string,
     quantity: PropTypes.number,
     price: PropTypes.number
   }),
-  onRemove: PropTypes.func
+  onRemove: PropTypes.func,
+  removeItem: PropTypes.func
 };
 
-export default CheckoutItem;
+const mapDispatchToProps = (dispatch) => ({
+  addItem: (newItem) => dispatch(addItemAction(newItem)),
+  removeItem: (item) => dispatch(removeItemAction(item))
+});
+
+export default connect(null, mapDispatchToProps)(CheckoutItem);
